d1982e23dff634caf91c3227891896cbc5e30460
Sam Mortimer <sam@mortimer.me.uk>
2017-09-15 17:42:28 -0700

    LineageSettings: Fixup ContentProvider query arguments
    As per similar changes made in
    frameworks/base/core/java/android/provider/Settings.java
    
    Whilst not strictly necessary, also renamed
    SELECT_VALUE to SELECT_VALUE_PROJECTION
    as it is now named in frameworks.
    
    Change-Id: Ib79bdc9a9c1f45d5f8405c7ecea30f68652f2839

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -132,7 +132,7 @@ public final class LineageSettings {
         private final String mVersionSystemProperty;
         private final Uri mUri;
 
-        private static final String[] SELECT_VALUE =
+        private static final String[] SELECT_VALUE_PROJECTION =
                 new String[] { Settings.NameValueTable.VALUE };
         private static final String NAME_EQ_PLACEHOLDER = "name=?";
 
@@ -264,8 +264,9 @@ public final class LineageSettings {
 
             Cursor c = null;
             try {
-                c = cp.query(cr.getPackageName(), mUri, SELECT_VALUE, NAME_EQ_PLACEHOLDER,
-                        new String[]{name}, null, null);
+                Bundle queryArgs = ContentResolver.createSqlQueryBundle(
+                        NAME_EQ_PLACEHOLDER, new String[]{name}, null);
+                c = cp.query(cr.getPackageName(), mUri, SELECT_VALUE_PROJECTION, queryArgs, null);
                 if (c == null) {
                     Log.w(TAG, "Can't get key " + name + " from " + mUri);
                     return null;
