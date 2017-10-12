68ed16aece1f31ae26b61101df89b7164db4e4c4
Sam Mortimer <sam@mortimer.me.uk>
2017-10-11 18:45:49 -0700

    lineage-sdk internal: remove deviceSupportsLte()
    It's no longer used anywhere and has a dependency
    on frameworks code that we don't want to add to
    lineage-15.0.
    
    Change-Id: Ib25bc214df658e7576e63c73a9c23b7e87ea71d8

--- sdk/src/java/org/lineageos/internal/util/QSUtils.java
+++ sdk/src/java/org/lineageos/internal/util/QSUtils.java
@@ -236,14 +236,6 @@ public class QSUtils {
         ctx.getContentResolver().unregisterContentObserver(observer);
     }
 
-
-    public static boolean deviceSupportsLte(Context ctx) {
-        final TelephonyManager tm = (TelephonyManager)
-                ctx.getSystemService(Context.TELEPHONY_SERVICE);
-        return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE)
-                || tm.getLteOnGsmMode() != 0;
-    }
-
     public static boolean deviceSupportsDdsSupported(Context context) {
         TelephonyManager tm = (TelephonyManager)
                 context.getSystemService(Context.TELEPHONY_SERVICE);
