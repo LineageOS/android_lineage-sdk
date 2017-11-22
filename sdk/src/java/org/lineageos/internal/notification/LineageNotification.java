ce3755438249e549d9ffb64e2223d61a5a1cbfba
Sam Mortimer <sam@mortimer.me.uk>
2017-11-21 23:26:59 -0800

    lineage-sdk: DnD light brightness control for battery and notifications
    *) Support separate normal and dnd led brightness levels for battery
       and notifications.
    
    *) Move lineage-specific notification bundle extras definitions from fw/b
       to LineageNotification here in the sdk.
    
    *) In addition to the existing bundle extra EXTRA_FORCE_SHOW_LIGHTS, add
       a new extra EXTRA_FORCE_LIGHT_BRIGHTNESS that can be used to override
       the brightness level set by a Lineage system setting brightness level
       on a per notification basis.  This is used by the brightness sliders
       in LineageParts that otherwise would have to juggle changing / restoring
       the system led brightness setting whenever the a slider is on display.
    
    *) Disable all lights in dnd mode when lineage setting ZEN_ALLOW_LIGHTS
       is set to 0.
    
    Change-Id: I917f402a291682b582f68c8324a33c461357dad9

---
+++ sdk/src/java/org/lineageos/internal/notification/LineageNotification.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.internal.notification;
+
+public class LineageNotification {
+
+    ///////////////////////////////////////////////////
+    // Lineage-specific Notification bundle extras
+    ///////////////////////////////////////////////////
+
+    /**
+     * Used by light picker in Settings to force
+     * notification lights on when screen is on.
+     */
+    public static final String EXTRA_FORCE_SHOW_LIGHTS = "lineage.forceShowLights";
+
+    /**
+     * Used by light picker in Settings to force
+     * a specific light brightness.
+     */
+    public static final String EXTRA_FORCE_LIGHT_BRIGHTNESS = "lineage.forceLightBrightness";
+}
