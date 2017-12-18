5539c469be83b26fd953d576bf0bd0ebbf5085cb
Bruno Martins <bgcngm@gmail.com>
2017-12-18 14:53:01 +0000

    lineage-sdk: Import device hardware keys configs and constants
     * Used to "live" in fw/b since ever, but will be here from now on.
    
    Change-Id: I3469a4084590c96f41c6ff128cf008143d315728

---
+++ sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.internal.util;
+
+public class DeviceKeysConstants {
+    // Masks for checking presence of hardware keys.
+    // Must match values in:
+    //   lineage/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
+}
+
