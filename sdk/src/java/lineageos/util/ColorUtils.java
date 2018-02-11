c439c514138e437570b3ddba8affca0a29861b2f
Sam Mortimer <sam@mortimer.me.uk>
2018-02-11 01:27:23 -0800

    lineage-sdk: Set drawable bounds in generateAlertColorFromDrawable()
    *) Android docs say this is required.
    
    *) Fixes generating alert color from oreo gmail app (and probably
       many others).
    
    Change-Id: Iad9a8ea82531c1cf844fae5456fd998a4b486165

--- sdk/src/java/lineageos/util/ColorUtils.java
+++ sdk/src/java/lineageos/util/ColorUtils.java
@@ -277,6 +277,7 @@ public class ColorUtils {
                                          Math.max(1, height),
                                          Bitmap.Config.ARGB_8888);
             Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
             drawable.draw(canvas);
         }
 
