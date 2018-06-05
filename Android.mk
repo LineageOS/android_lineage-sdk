cf4d8c5372489fe5757c5a50ea7f947cbbaebaeb
Michael W <baddaemon87@gmail.com>
2018-06-05 19:47:22 +0200

    lineage-sdk: Introduce TelephonyExtUtils
    Change-Id: Ifdd18dd6e89e23c5b804c3454ee8473a5920b562

--- Android.mk
+++ Android.mk
@@ -46,6 +46,7 @@ lineage_sdk_LOCAL_JAVA_LIBRARIES := \
 
 LOCAL_JAVA_LIBRARIES := \
     services \
+    telephony-ext \
     org.lineageos.hardware \
     $(lineage_sdk_LOCAL_JAVA_LIBRARIES)
 
@@ -194,6 +195,7 @@ LOCAL_INTERMEDIATE_SOURCES := \
     $(lineage_sdk_LOCAL_INTERMEDIATE_SOURCES)
 
 LOCAL_JAVA_LIBRARIES := \
+    telephony-ext \
     $(lineage_sdk_LOCAL_JAVA_LIBRARIES)
 
 $(full_target): $(lineage_framework_built) $(gen)
