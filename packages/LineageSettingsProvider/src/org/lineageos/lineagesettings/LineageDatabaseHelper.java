9a5742fc3313e91a17d9d7c7f9753cac835617c9
LuK1337 <priv.luk@gmail.com>
2018-07-15 17:25:46 +0200

    sdk: Add default setting for clock position
    * Will be used by devices with notch.
    
    Change-Id: Ie2af3f4741637656054d97ecbb5f20553c6001fd

--- packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
+++ packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
@@ -470,6 +470,9 @@ public class LineageDatabaseHelper extends SQLiteOpenHelper{
             loadIntegerSetting(stmt, LineageSettings.System.STATUS_BAR_BATTERY_STYLE,
                     R.integer.def_battery_style);
 
+            loadIntegerSetting(stmt, LineageSettings.System.STATUS_BAR_CLOCK,
+                    R.integer.def_clock_position);
+
             if (mContext.getResources().getBoolean(R.bool.def_notification_pulse_custom_enable)) {
                 loadStringSetting(stmt, LineageSettings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES,
                         R.string.def_notification_pulse_custom_value);
