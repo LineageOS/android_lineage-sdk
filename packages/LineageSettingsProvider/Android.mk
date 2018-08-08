14c04efd60f7a5a0970fa3e5a8ec5ed84efffd04
Sam Mortimer <sam@mortimer.me.uk>
2018-08-08 15:12:02 -0700

    lineage-sdk: Set LOCAL_PRIVATE_PLATFORM_APIS := true
    Change-Id: I06b9374ccee78550fcc26d876a9e4ea50198bbfe

--- packages/LineageSettingsProvider/Android.mk
+++ packages/LineageSettingsProvider/Android.mk
@@ -27,6 +27,8 @@ LOCAL_PACKAGE_NAME := LineageSettingsProvider
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat \
