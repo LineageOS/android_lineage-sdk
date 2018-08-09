c3880ff28065716df0355294e63b9929e79b53b4
Sam Mortimer <sam@mortimer.me.uk>
2018-08-09 11:10:44 -0700

    lineage-sdk: Use PreferenceDataStore for lineage-sdk preferences
    Replaces the need for:
    https://github.com/LineageOS/android_frameworks_support/commit/32e67e60201feee32b5af8ef4bb66c98292cd3b7
    
    *) frameworks/support are now built with gradle and are included as prebuilts.
    
    *) API level 26 (8.0) introduced PreferenceDataStorage which can be used
       to implement what we need without touching the support library.
    
    Change-Id: Ide3417019ccf8bde5dca3b2aec3d2075e7c8b597

--- sdk/src/java/lineageos/preference/SecureSettingSwitchPreference.java
+++ sdk/src/java/lineageos/preference/SecureSettingSwitchPreference.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2014-2016 The CyanogenMod project
+ * Copyright (C) 2018 The LineageOS project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,29 +36,18 @@ public class SecureSettingSwitchPreference extends SelfRemovingSwitchPreference 
     }
 
     @Override
-    protected boolean persistBoolean(boolean value) {
-        if (shouldPersist()) {
-            if (value == getPersistedBoolean(!value)) {
-                // It's already there, so the same as persisting
-                return true;
-            }
-            Settings.Secure.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
-        if (!shouldPersist()) {
-            return defaultReturnValue;
-        }
-        return Settings.Secure.getInt(getContext().getContentResolver(),
-                getKey(), defaultReturnValue ? 1 : 0) != 0;
-    }
-
-    @Override
     protected boolean isPersisted() {
         return Settings.Secure.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+
+    @Override
+    protected void putBoolean(String key, boolean value) {
+        Settings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+    }
+
+    @Override
+    protected boolean getBoolean(String key, boolean defaultValue) {
+        return Settings.Secure.getInt(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0) != 0;
     }
 }
