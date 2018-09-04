84d90ce2a1c495acbb269f50adc805afa010eb8f
Sam Mortimer <sam@mortimer.me.uk>
2018-09-03 18:32:40 -0700

    lineage-sdk: Bump PREF_HAS_MIGRATED_LINEAGE_SETTINGS for 16.0
    Change-Id: Ia88d36533c0b0a2dd8ca18e93de5d03441dd8092

--- packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageSettingsProvider.java
+++ packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageSettingsProvider.java
@@ -66,7 +66,7 @@ public class LineageSettingsProvider extends ContentProvider {
     private static final boolean USER_CHECK_THROWS = true;
 
     public static final String PREF_HAS_MIGRATED_LINEAGE_SETTINGS =
-            "migrated_settings_to_lineage_15_1";
+            "migrated_settings_to_lineage_16_0";
 
     private static final Bundle NULL_SETTING = Bundle.forPair("value", null);
 
