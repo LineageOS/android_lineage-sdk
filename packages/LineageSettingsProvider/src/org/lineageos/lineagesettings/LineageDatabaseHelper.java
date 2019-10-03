5ef2aee7738f2008182c3d0939ce1ce6d418dd9c
Chippa-a <vusal1372@gmail.com>
2019-10-03 18:40:45 +0300

    lineage-sdk: Update path for custom charging sound
    Audio files were moved to /product on Android 10.
    Moreover, align with AOSP and use the wireless sound
    for wired usecase too.
    
    In order to perform upgrade properly, handle the migration
    for custom charging sound setting.
    
    Change-Id: Ifb2085e3f56d777091d66544a72cdd75c843fb56

--- packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
+++ packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
@@ -48,7 +48,7 @@ public class LineageDatabaseHelper extends SQLiteOpenHelper{
     private static final boolean LOCAL_LOGV = false;
 
     private static final String DATABASE_NAME = "lineagesettings.db";
-    private static final int DATABASE_VERSION = 12;
+    private static final int DATABASE_VERSION = 13;
 
     private static final String DATABASE_NAME_OLD = "cmsettings.db";
 
@@ -425,6 +425,26 @@ public class LineageDatabaseHelper extends SQLiteOpenHelper{
                 }
             }
             upgradeVersion = 12;
+        }
+
+        if (upgradeVersion < 13) {
+            // Update custom charging sound setting
+            if (mUserHandle == UserHandle.USER_OWNER) {
+                db.beginTransaction();
+                SQLiteStatement stmt = null;
+                try {
+                    stmt = db.compileStatement("UPDATE global SET value=? WHERE name=?");
+                    stmt.bindString(1, mContext.getResources()
+                            .getString(R.string.def_power_notifications_ringtone));
+                    stmt.bindString(2, LineageSettings.Global.POWER_NOTIFICATIONS_RINGTONE);
+                    stmt.execute();
+                    db.setTransactionSuccessful();
+                } finally {
+                    if (stmt != null) stmt.close();
+                    db.endTransaction();
+                }
+            }
+            upgradeVersion = 13;
         }
         // *** Remember to update DATABASE_VERSION above!
     }
