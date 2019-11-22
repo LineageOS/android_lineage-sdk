e8da3a9ed5f73349e1cccf0c741b641bcca9f075
dianlujitao <dianlujitao@lineageos.org>
2019-11-22 21:05:43 +0800

    sdk: Add lineageadbroot service
    Change-Id: Iff6b8ca3da5c56e29a5c569a514077fa9ededd84

--- lineage/lib/main/java/org/lineageos/platform/internal/LineageSettingsService.java
+++ lineage/lib/main/java/org/lineageos/platform/internal/LineageSettingsService.java
@@ -17,10 +17,17 @@
 package org.lineageos.platform.internal;
 
 import android.content.Context;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Process;
 import android.os.SystemProperties;
+import android.os.SystemService;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import lineageos.app.LineageContextConstants;
 import lineageos.providers.LineageSettings;
+import lineageos.system.IAdbRootService;
 
 /** @hide */
 public class LineageSettingsService extends LineageSystemService {
@@ -51,5 +58,74 @@ public class LineageSettingsService extends LineageSystemService {
 
     @Override
     public void onStart() {
+        publishBinderService(LineageContextConstants.LINEAGE_ADBROOT_SERVICE, mAdbRootService);
     }
+
+    /* Utils */
+
+    private boolean isCallerSystem() {
+        final int callingUid = Binder.getCallingUid();
+        return UserHandle.isSameApp(callingUid, Process.SYSTEM_UID);
+    }
+
+    private boolean isCallerShell() {
+        final int callingUid = Binder.getCallingUid();
+        return callingUid == Process.SHELL_UID || callingUid == Process.ROOT_UID;
+    }
+
+    private void enforceSystem() {
+        if (!isCallerSystem()) {
+            throw new SecurityException("Caller must be system");
+        }
+    }
+
+    private void enforceSystemOrShell() {
+        if (!(isCallerSystem() || isCallerShell())) {
+            throw new SecurityException("Caller must be system or shell");
+        }
+    }
+
+    /* Service */
+
+    private final IBinder mAdbRootService = new IAdbRootService.Stub() {
+        private final Object mLock = new Object();
+
+        private void setEnabledLocked(boolean enabled) {
+            final boolean curEnabled = getEnabledLocked();
+            if (curEnabled != enabled) {
+                LineageSettings.Secure.putInt(mContext.getContentResolver(),
+                        LineageSettings.Secure.ADB_ROOT_ENABLED, enabled ? 1 : 0);
+
+                // Turning off adb root, restart adbd.
+                if (curEnabled) {
+                    SystemProperties.set("lineage.service.adb.root", "0");
+                    SystemService.restart("adbd");
+                }
+            }
+        }
+
+        private boolean getEnabledLocked() {
+            return LineageSettings.Secure.getInt(mContext.getContentResolver(),
+                    LineageSettings.Secure.ADB_ROOT_ENABLED, 0) != 0;
+        }
+
+        @Override
+        public void setEnabled(boolean enabled) {
+            enforceSystem();
+            synchronized (mLock) {
+                setEnabledLocked(enabled);
+            }
+        }
+
+        @Override
+        public boolean getEnabled() {
+            enforceSystemOrShell();
+            synchronized (mLock) {
+                final long token = clearCallingIdentity();
+                final boolean result = getEnabledLocked();
+                restoreCallingIdentity(token);
+                return result;
+            }
+        }
+    };
 }
