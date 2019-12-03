d0702b12883d437feabe8f8696d8109c0107ac3b
Wang Han <416810799@qq.com>
2019-12-03 15:24:06 +0800

    lineage-sdk: Drop root status of Trust
    Change-Id: Iaec0b2a8a781a0e9d301a21a3af5e4362a5d6630

--- sdk/src/java/lineageos/trust/TrustInterface.java
+++ sdk/src/java/lineageos/trust/TrustInterface.java
@@ -75,18 +75,6 @@ public class TrustInterface {
     public static final int TRUST_FEATURE_SELINUX = 0;
 
     /**
-     * Trust feature indicator: Root access
-     *
-     * Possible status:
-     *    * {@link #TRUST_FEATURE_LEVEL_GOOD}: disabled
-     *    * {@link #TRUST_FEATURE_LEVEL_POOR}: ADB only
-     *    * {@link #TRUST_FEATURE_LEVEL_BAD}: apps and ADB
-     *
-     * @see #getLevelForFeature
-     */
-    public static final int TRUST_FEATURE_ROOT = 1;
-
-    /**
      * Trust feature indicator: Platform Security patches
      *
      * Possible status:
@@ -151,16 +139,6 @@ public class TrustInterface {
     public static final int TRUST_WARN_SELINUX = 1;
 
     /**
-     * Trust warning: Root access
-     *
-     * When {@link #TRUST_FEATURE_ROOT} is not {@link #TRUST_FEATURE_LEVEL_GOOD}
-     * notify the user about the issue
-     *
-     * @see #postNotificationForFeature
-     */
-    public static final int TRUST_WARN_ROOT = 1 << 1;
-
-    /**
      * Trust warning: Public Key build signature
      *
      * When {@link #TRUST_FEATURE_KEYS} is not {@link #TRUST_FEATURE_LEVEL_GOOD}
@@ -180,7 +158,6 @@ public class TrustInterface {
      */
     public static final int TRUST_WARN_MAX_VALUE =
             TRUST_WARN_SELINUX |
-            TRUST_WARN_ROOT |
             TRUST_WARN_PUBLIC_KEY;
 
     private static final String TAG = "TrustInterface";
