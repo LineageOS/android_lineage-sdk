5cb3e512cd7b52508c0dfb4ba61508f0a24b0dc7
BeYkeRYkt <beykerykt@gmail.com>
2020-01-17 13:41:13 +0900

    PerformanceManager: Fix applying power profile in low power mode
    * Fixes the wrong power profile after rebooting the device, if before that low
      power mode was turned on.
    
    Change-Id: I6008433b5e304a0f168ba9c7eeb189f4d3e83347

--- lineage/lib/main/java/org/lineageos/platform/internal/PerformanceManagerService.java
+++ lineage/lib/main/java/org/lineageos/platform/internal/PerformanceManagerService.java
@@ -155,7 +155,7 @@ public class PerformanceManagerService extends LineageSystemService {
 
                 synchronized (mLock) {
                     mMpctlReady = true;
-                    setPowerProfileLocked(mUserProfile, false);
+                    applyProfileLocked();
                 }
             });
             mWaitMpctlThread.setDaemon(true);
