b17cb0310261b68aef286bd00e0f5b0524933672
Linux4 <tim@linux4.de>
2020-04-20 18:00:38 +0200

    lineage-sdk: add powershare qs tile
    Change-Id: Ia324041843b7314881d9958f81fdc45a92b56ea0

--- sdk/src/java/org/lineageos/internal/logging/LineageMetricsLogger.java
+++ sdk/src/java/org/lineageos/internal/logging/LineageMetricsLogger.java
@@ -72,4 +72,5 @@ public class LineageMetricsLogger extends MetricsLogger {
     public static final int TILE_LOCATION_DETAIL = BASE + 44;
     public static final int TILE_READING_MODE = BASE + 45;
     public static final int TILE_AOD = BASE + 46;
+    public static final int TILE_POWERSHARE = BASE + 47;
 }
