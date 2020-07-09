4c52e250eb0f3f952c6bb538e6743f55c6d880b4
TheScarastic <warabhishek@gmail.com>
2020-07-09 12:26:44 +0000

    lineage-sdk: Screen off fod [2/3]
    Change-Id: I0bbc31778cc6918ba5de699318e750ce6a63f2ba

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -2163,6 +2163,15 @@ public final class LineageSettings {
                 sBooleanValidator;
 
         /**
+         * Screen off fod
+         * @hide
+         */
+        public static final String FOD_GESTURE = "fod_gesture";
+
+        /** @hide */
+        public static final Validator FOD_GESTURE_VALIDATOR = sBooleanValidator;
+
+        /**
          * I can haz more bukkits
          * @hide
          */
@@ -2449,6 +2458,7 @@ public final class LineageSettings {
                     CLICK_PARTIAL_SCREENSHOT_VALIDATOR);
             VALIDATORS.put(__MAGICAL_TEST_PASSING_ENABLER,
                     __MAGICAL_TEST_PASSING_ENABLER_VALIDATOR);
+            VALIDATORS.put(FOD_GESTURE, FOD_GESTURE_VALIDATOR);
         };
         // endregion
     }
