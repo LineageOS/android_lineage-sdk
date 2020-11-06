d8742f0727aad8187a903a0afbafbc909edf1d1c
LuK1337 <priv.luk@gmail.com>
2020-11-06 20:04:03 +0100

    sdk: Deprecate POWER_MENU_ACTIONS
    Change-Id: I35b0d1123a427598e548cf865328a7c9423cbbca

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -2822,8 +2822,10 @@ public final class LineageSettings {
 
         /**
          * String to contain power menu actions
+         * @deprecated
          * @hide
          */
+        @Deprecated
         public static final String POWER_MENU_ACTIONS = "power_menu_actions";
 
         /**
