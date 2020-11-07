8876f8748035a28387695bc41c85e528080df5c2
Arbri çoçka <35626219+cocka11@users.noreply.github.com>
2020-11-07 07:41:16 +0100

    Trust: Actually cancel notification if the setting is disabled

--- lineage/lib/main/java/org/lineageos/platform/internal/TrustInterfaceService.java
+++ lineage/lib/main/java/org/lineageos/platform/internal/TrustInterfaceService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2019 The LineageOS Project
+ * Copyright (C) 2018-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -143,7 +143,7 @@ public class TrustInterfaceService extends LineageSystemService {
     }
 
     private boolean removeNotificationForFeatureInternal(int feature) {
-        if (!isWarningAllowed(feature)) {
+        if (isWarningAllowed(feature)) {
             return false;
         }
 
