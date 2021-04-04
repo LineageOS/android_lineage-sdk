3ebff264009baf6ed19b4619ac6b7f2af1679ad8
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-04-04 16:09:33 +0400

    lineage-sdk: Add GlobalActions service
    The service will offer updateUserConfig, getUserConfig, and
    userConfigContains methods, which will be used from LineageParts.
    The service will also observe Settings.Global.BUGREPORT_IN_POWER_MENU
    and Settings.Secure.LOCKDOWN_IN_POWER_MENU to update the GlobalActions
    dialog when these are updated.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I46663ab1ffe6f8d8b2a3dc7dbe54a65b7a62046a

---
+++ lineage/lib/main/java/org/lineageos/platform/internal/GlobalActionsManagerService.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.platform.internal;
+
+import static lineageos.providers.LineageSettings.Secure.POWER_MENU_ACTIONS;
+import static lineageos.providers.LineageSettings.Secure.getStringForUser;
+import static lineageos.providers.LineageSettings.Secure.putStringForUser;
+
+import static org.lineageos.internal.util.PowerMenuConstants.GLOBAL_ACTION_KEY_BUGREPORT;
+import static org.lineageos.internal.util.PowerMenuConstants.GLOBAL_ACTION_KEY_LOCKDOWN;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Process;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import lineageos.app.LineageContextConstants;
+import lineageos.app.IGlobalActionsManager;
+
+import org.lineageos.internal.util.PowerMenuConstants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @hide
+ */
+public class GlobalActionsManagerService extends LineageSystemService {
+
+    private static final String TAG = "GlobalActionsManager";
+
+    private static final boolean DEBUG = false;
+
+    private final Context mContext;
+    private final ContentResolver mContentResolver;
+
+    private final List<String> mLocalUserConfig = new ArrayList<String>();
+
+    // Observes user-controlled settings
+    private GlobalActionsSettingsObserver mObserver;
+
+    public GlobalActionsManagerService(Context context) {
+        super(context);
+
+        mContext = context;
+        mContentResolver = mContext.getContentResolver();
+    }
+
+    private class GlobalActionsSettingsObserver extends ContentObserver {
+
+        private final Uri BUGREPORT_URI =
+                Settings.Global.getUriFor(Settings.Global.BUGREPORT_IN_POWER_MENU);
+        private final Uri LOCKDOWN_URI =
+                Settings.Secure.getUriFor(Settings.Secure.LOCKDOWN_IN_POWER_MENU);
+
+        public GlobalActionsSettingsObserver(Context context, Handler handler) {
+            super(handler);
+        }
+
+        public void observe(boolean enabled) {
+            if (enabled) {
+                mContentResolver.registerContentObserver(BUGREPORT_URI, false, this);
+                mContentResolver.registerContentObserver(LOCKDOWN_URI, false, this);
+            } else {
+                mContentResolver.unregisterContentObserver(this);
+            }
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateUserConfig(Settings.Global.getInt(mContentResolver,
+                    Settings.Global.BUGREPORT_IN_POWER_MENU, 0) == 1, GLOBAL_ACTION_KEY_BUGREPORT);
+
+            updateUserConfig(Settings.Secure.getInt(mContentResolver,
+                    Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0) == 1, GLOBAL_ACTION_KEY_LOCKDOWN);
+        }
+    };
+
+    private void populateUserConfig() {
+        mLocalUserConfig.clear();
+
+        String savedActions = getStringForUser(mContentResolver,
+                POWER_MENU_ACTIONS, UserHandle.USER_CURRENT);
+
+        if (savedActions == null) {
+            mLocalUserConfig.addAll(Arrays.asList(mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_globalActionsList)));
+        } else {
+            mLocalUserConfig.addAll(Arrays.asList(savedActions.split("\\|")));
+        }
+    }
+
+    private void updateUserConfig(boolean enabled, String action) {
+        if (enabled) {
+            if (!mLocalUserConfig.contains(action)) {
+                mLocalUserConfig.add(action);
+            }
+        } else {
+            if (mLocalUserConfig.contains(action)) {
+                mLocalUserConfig.remove(action);
+            }
+        }
+    }
+
+    private void saveUserConfig() {
+        List<String> actions = new ArrayList<String>();
+        for (String action : PowerMenuConstants.getAllActions()) {
+            if (mLocalUserConfig.contains(action)) {
+                actions.add(action);
+            }
+        }
+
+        String s = String.join("|", actions);
+
+        putStringForUser(mContentResolver,
+                POWER_MENU_ACTIONS, s, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public String getFeatureDeclaration() {
+        return LineageContextConstants.Features.GLOBALACTIONS;
+    }
+
+    @Override
+    public void onStart() {
+        publishBinderService(LineageContextConstants.LINEAGE_GLOBALACTIONS_SERVICE, mBinder);
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            populateUserConfig();
+
+            mObserver = new GlobalActionsSettingsObserver(mContext, null);
+            mObserver.observe(true);
+        }
+    }
+
+    private final IBinder mBinder = new IGlobalActionsManager.Stub() {
+
+        @Override
+        public void updateUserConfig(boolean enabled, String action) {
+            updateUserConfig(enabled, action);
+        }
+
+        @Override
+        public List<String> getUserConfig() {
+            return mLocalUserConfig;
+        }
+
+        @Override
+        public boolean userConfigContains(String preference) {
+            return mLocalUserConfig.contains(preference);
+        }
+    };
+}
