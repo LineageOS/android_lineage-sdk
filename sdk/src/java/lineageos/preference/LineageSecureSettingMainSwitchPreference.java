987199d618422b91391de28846184b32f988f27b
Bruno Martins <bgcngm@gmail.com>
2021-12-31 19:43:27 +0000

    preference: Introduce {Global,Secure,System}MainSwitchPreference
    Change-Id: I04cf71ea60b3ecde3c8be039ab320e8730ca4b3f

---
+++ sdk/src/java/lineageos/preference/LineageSecureSettingMainSwitchPreference.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lineageos.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+import lineageos.providers.LineageSettings;
+
+public class LineageSecureSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public LineageSecureSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public LineageSecureSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public LineageSecureSettingMainSwitchPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public void putBoolean(String key, boolean value) {
+        LineageSettings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+    }
+
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return LineageSettings.Secure.getInt(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0) != 0;
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            putBoolean(key, value);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return getBoolean(key, defaultValue);
+        }
+    }
+}
---
+++ sdk/src/java/lineageos/preference/SecureSettingMainSwitchPreference.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lineageos.preference;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class SecureSettingMainSwitchPreference extends MainSwitchPreference {
+
+    public SecureSettingMainSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SecureSettingMainSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SecureSettingMainSwitchPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    protected void putBoolean(String key, boolean value) {
+        Settings.Secure.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+    }
+
+    protected boolean getBoolean(String key, boolean defaultValue) {
+        return Settings.Secure.getInt(getContext().getContentResolver(),
+                key, defaultValue ? 1 : 0) != 0;
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            putBoolean(key, value);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return getBoolean(key, defaultValue);
+        }
+    }
+}
