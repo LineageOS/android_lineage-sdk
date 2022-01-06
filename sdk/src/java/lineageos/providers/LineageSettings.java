2f47d9b7af4eb3122be115c2b44d82947fd70810
Pranav Vashi <neobuddy89@gmail.com>
2022-01-06 22:49:52 +0530

    Add setting for brightness slider position
    Change-Id: I89ef42385fa71d2bb7294a1682600c91d9c90989
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -2719,6 +2719,13 @@ public final class LineageSettings {
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
         /**
+         * Whether to show the brightness slider in quick settings panel.
+         * 0 = Top, 1 = Bottom
+         * @hide
+         */
+        public static final String QS_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+
+        /**
          * Whether to show the auto brightness icon in quick settings panel.
          * @hide
          */
