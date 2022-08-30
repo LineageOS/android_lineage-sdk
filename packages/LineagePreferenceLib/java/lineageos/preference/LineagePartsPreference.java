1dcef2eaeec5be16ba01c56901a0cbbcc27bc305
LuK1337 <priv.luk@gmail.com>
2022-08-30 09:24:33 +0200

    sdk: Introduce LineageSettingsLib
    Because of some nested dependency breakage, we can no longer include
    android libraries in main SDK lib, therefore we are moving all
    preference related code to a separate package that Settings/LineageParts
    will depend on.
    
    Change-Id: Iea379a580a08ea13f71e3503aa096ba0ed7e7cef

--- packages/LineagePreferenceLib/java/lineageos/preference/LineagePartsPreference.java
+++ packages/LineagePreferenceLib/java/lineageos/preference/LineagePartsPreference.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.lineageos.internal.lineageparts;
+package lineageos.preference;
 
 import android.content.Context;
 import android.os.Bundle;
