06d202ead870a1f6755f22b8a2b4b64279973560
Luofan Chen <clfbbn@gmail.com>
2023-07-01 19:40:50 +0800

    wip: ChargingControl: Decouple charging control and main logic
    The existing Hardware Abstraction Layer (HAL) supports two distinct
    control modes: TOGGLE and DEADLINE, each offering unique capabilities.
    For instance, the TOGGLE mode allows for control over both charging
    time and limit, while the DEADLINE mode only enables control over the
    charging time. Managing these separate logic streams within a single
    ChargingControlController complicates the development process.
    
    This commit separates the specific charging control logic — determining
    what to send to the HAL—from the primary logic. The charging control
    module now offers providers tailored to each HAL-supported charging
    control mode, allowing for limit control, time control, or both. When
    required, the ChargingControlController invokes these specific providers.
    
    This separation simplifies the codebase. Moreover, when introducing a new
    mode in the HAL, developers only need to implement the corresponding
    provider's logic based on the mode's capabilities. And minimal changes
    are needed in the parimary logic.
    
    Change-Id: Ie40020c2df4141d4aa6385c8f5565821af942755

---
+++ lineage/lib/main/java/org/lineageos/platform/internal/health/provider/TimeProviderDeadline.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.platform.internal.health.provider;
+
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.io.PrintWriter;
+
+import vendor.lineage.health.ChargingControlSupportedMode;
+import vendor.lineage.health.IChargingControl;
+
+import static org.lineageos.platform.internal.health.ChargingControlController.msToString;
+
+public class TimeProviderDeadline extends ChargingControlProvider {
+
+    private long mSavedTargetTime;
+
+    public TimeProviderDeadline(IChargingControl chargingControl, Context context) {
+        super(chargingControl, context);
+    }
+
+    @Override
+    public boolean isSupported() {
+        return isModeSupported(ChargingControlSupportedMode.DEADLINE);
+    }
+
+    @Override
+    public boolean shouldAlwaysMonitorBattery() {
+        return false;
+    }
+
+    @Override
+    public ChargeControlInfo onBatteryChanged(float batteryPct, long startTime, long targetTime,
+            int configMode) {
+        long deadline = 0;
+        final long currentTime = System.currentTimeMillis();
+
+        if (currentTime < startTime) {
+            Log.i(TAG, "Not yet entering user set time-frame.");
+            disable();
+            return new ChargeControlInfo(false, false);
+        }
+
+        if (targetTime == mSavedTargetTime) {
+            return new ChargeControlInfo(true, false);
+        }
+
+        deadline = (targetTime - currentTime) / 1000;
+        Log.i(TAG, "Setting charge deadline: Current time: " + msToString(currentTime));
+        Log.i(TAG, "Setting charge deadline: Target time: " + msToString(targetTime));
+        Log.i(TAG, "Setting charge deadline: Deadline (seconds): " + deadline);
+
+        try {
+            mChargingControl.setChargingDeadline(deadline);
+            mSavedTargetTime = targetTime;
+        } catch (IllegalStateException | RemoteException | UnsupportedOperationException e) {
+            Log.e(TAG, "Failed to set charge deadline", e);
+        }
+
+        return new ChargeControlInfo(true, batteryPct == 100);
+    }
+
+    @Override
+    public void disable() {
+        try {
+            mChargingControl.setChargingDeadline(-1);
+            mSavedTargetTime = 0;
+        } catch (IllegalStateException | RemoteException | UnsupportedOperationException e) {
+            Log.e(TAG, "Failed to set charge deadline", e);
+        }
+    }
+
+    @Override
+    public void reset() {
+        mSavedTargetTime = 0;
+    }
+
+    @Override
+    public void dump(PrintWriter pw) {
+        pw.println("Provider: " + getClass().getName());
+        pw.println("  mSavedTargetTime: " + msToString(mSavedTargetTime, "UTC"));
+    }
+}
