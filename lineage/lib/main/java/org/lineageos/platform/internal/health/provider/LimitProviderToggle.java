86a3a497af7808bb1ad36691c784c90e9ea3a4a2
Luofan Chen <clfbbn@gmail.com>
2023-07-01 19:40:50 +0800

    ChargingControl: Decouple charging control and main logic
    The existing Hardware Abstraction Layer (HAL) supports two distinct
    control modes: TOGGLE and DEADLINE, each offering unique capabilities.
    For instance, the TOGGLE mode allows for control over both charging
    time and limit, while the DEADLINE mode only enables control over the
    charging time. Managing these separate logic streams within a single
    ChargingControlController complicates the development process.
    
    This commit separates the specific charging control logic — determining
    what to send to the HAL—from the primary logic. The charging control
    module now offers providers tailored to each HAL-supported charging
    control mode, allowing for limit control, time control, or both. When
    required, the ChargingControlController invokes these specific providers.
    
    This separation simplifies the codebase. Moreover, when introducing a new
    mode in the HAL, developers only need to implement the corresponding
    provider's logic based on the mode's capabilities. And minimal changes
    are needed in the primary logic.
    
    Change-Id: Ie40020c2df4141d4aa6385c8f5565821af942755

---
+++ lineage/lib/main/java/org/lineageos/platform/internal/health/provider/LimitProviderToggle.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.platform.internal.health.provider;
+
+import android.content.Context;
+
+import java.io.PrintWriter;
+
+import vendor.lineage.health.IChargingControl;
+import vendor.lineage.health.ChargingControlSupportedMode;
+
+public class LimitProviderToggle extends ChargingControlToggleProvider {
+
+    private boolean isLimitSet;
+    protected int mChargingLimitMargin;
+
+    public LimitProviderToggle(IChargingControl chargingControl,
+            Context context) {
+        super(chargingControl, context);
+        mChargingLimitMargin = 1;
+    }
+
+    @Override
+    public boolean isSupported() {
+        return isModeSupported(
+                ChargingControlSupportedMode.TOGGLE & ChargingControlSupportedMode.BYPASS);
+    }
+
+    @Override
+    public boolean shouldAlwaysMonitorBattery() {
+        return false;
+    }
+
+    private boolean shouldStopCharging(float currentPct, int targetPct) {
+        if (isLimitSet) {
+            return currentPct >= targetPct - mChargingLimitMargin;
+        }
+
+        return currentPct >= targetPct;
+    }
+
+    @Override
+    public ChargeControlInfo onBatteryChanged(float currentPct, int targetPct) {
+        boolean shouldStopCharging = shouldStopCharging(currentPct, targetPct);
+        if (setChargingEnabled(!shouldStopCharging)) {
+            isLimitSet = shouldStopCharging;
+        }
+
+        return new ChargeControlInfo(true, isLimitSet);
+    }
+
+    @Override
+    public void onReset() {
+        isLimitSet = false;
+    }
+
+    @Override
+    public void dump(PrintWriter pw) {
+        pw.println("Provider: " + getClass().getName());
+        pw.println("  isLimitSet: " + isLimitSet);
+        pw.println("  mChargeLimitMargin: " + mChargingLimitMargin);
+    }
+}
