9e850826fc9acb782c92dfa83a078292b708bb75
Tommaso Fonda <fondatommaso2@gmail.com>
2023-09-16 18:48:46 +0200

    trust: Fix variable usage before null check
    Change-Id: I8258b647b2cc32daa759562a0b5a03901b0edcd4

--- sdk/src/java/lineageos/trust/TrustInterface.java
+++ sdk/src/java/lineageos/trust/TrustInterface.java
@@ -198,7 +198,6 @@ public class TrustInterface {
             return sService;
         }
         IBinder b = ServiceManager.getService(LineageContextConstants.LINEAGE_TRUST_INTERFACE);
-        sService = ITrustInterface.Stub.asInterface(b);
 
         if (b == null) {
             Log.e(TAG, "null service. SAD!");
