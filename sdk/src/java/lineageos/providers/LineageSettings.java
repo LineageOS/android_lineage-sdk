46b59c0f1f8dcb1f8d20b7adfb3e62a49eac42eb
mikooomich <mikooomichael@gmail.com>
2024-02-05 20:46:27 -0500

    LineageSettings: Add per-app volume key
    Change-Id: I8f8361ed4c2af36c1aca27dde3a2e61f51d6b304

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -1045,6 +1045,16 @@ public final class LineageSettings {
                 sBooleanValidator;
 
         /**
+         * Show per-app volume controls in volume hud
+         * Defaults to 0 - Per-app volume controls hidden
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /** @hide */
+        public static final Validator SHOW_APP_VOLUME_VALIDATOR =
+                sBooleanValidator;
+
+        /**
          * Navigation controls to Use
          */
         public static final String NAV_BUTTONS = "nav_buttons";
@@ -2140,6 +2150,7 @@ public final class LineageSettings {
                     INCREASING_RING_RAMP_UP_TIME_VALIDATOR);
             VALIDATORS.put(VOLUME_ADJUST_SOUNDS_ENABLED,
                     VOLUME_ADJUST_SOUNDS_ENABLED_VALIDATOR);
+            VALIDATORS.put(SHOW_APP_VOLUME, SHOW_APP_VOLUME_VALIDATOR);
             VALIDATORS.put(NAV_BUTTONS, NAV_BUTTONS_VALIDATOR);
             VALIDATORS.put(NAVIGATION_BAR_MENU_ARROW_KEYS,
                     NAVIGATION_BAR_MENU_ARROW_KEYS_VALIDATOR);
