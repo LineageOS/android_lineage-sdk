baebbbf0d1b584e6c4787f5e947ba7c0b47db078
Michael Bestas <mkbestas@lineageos.org>
2024-05-29 14:39:09 +0300

    LineageSettingsProvider: Relocate loadRestrictedNetworkingModeSetting call
    It shouldn't be in a SQLiteStatement block.
    
    Change-Id: I342af1f9e52150bd87aa20701a7109db3fa6f2bb

--- packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
+++ packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
@@ -387,6 +387,7 @@ public class LineageDatabaseHelper extends SQLiteOpenHelper{
         // The global table only exists for the 'owner' user
         if (mUserHandle == UserHandle.USER_SYSTEM) {
             loadGlobalSettings(db);
+            loadRestrictedNetworkingModeSetting();
         }
     }
 
@@ -466,7 +467,6 @@ public class LineageDatabaseHelper extends SQLiteOpenHelper{
             stmt = db.compileStatement("INSERT OR IGNORE INTO global(name,value)"
                     + " VALUES(?,?);");
             // Global
-            loadRestrictedNetworkingModeSetting();
         } finally {
             if (stmt != null) stmt.close();
         }
