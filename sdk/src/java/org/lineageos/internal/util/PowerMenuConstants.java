d97af7af3bc272bf3aaead72c6672040e1ce54c4
Michael Bestas <mkbestas@lineageos.org>
2024-09-23 04:11:06 +0300

    sdk: Add GLOBAL_ACTION_KEY_SYSTEM_UPDATE
    Change-Id: Ib52da4a5f43489a0d87f071e124871bc3da2bf8f

--- sdk/src/java/org/lineageos/internal/util/PowerMenuConstants.java
+++ sdk/src/java/org/lineageos/internal/util/PowerMenuConstants.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- * Copyright (C) 2017-2022 The LineageOS Project
+ * Copyright (C) 2017-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,7 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_LOGOUT = "logout";
     public static final String GLOBAL_ACTION_KEY_EMERGENCY = "emergency";
     public static final String GLOBAL_ACTION_KEY_DEVICECONTROLS = "devicecontrols";
+    public static final String GLOBAL_ACTION_KEY_SYSTEM_UPDATE = "system_update";
 
     /**
      * Advanced restart menu actions
@@ -57,6 +58,7 @@ public class PowerMenuConstants {
         GLOBAL_ACTION_KEY_ASSIST,
         GLOBAL_ACTION_KEY_DEVICECONTROLS,
         GLOBAL_ACTION_KEY_LOGOUT,
+        GLOBAL_ACTION_KEY_SYSTEM_UPDATE,
     };
 
     public static String[] getAllActions() {
