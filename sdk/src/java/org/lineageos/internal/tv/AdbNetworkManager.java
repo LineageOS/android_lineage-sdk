5d0b594f74a6fe499327b64368838c58e9f858ec
Timi <timi.rautamaki@gmail.com>
2025-01-18 11:56:04 +0200

    sdk: AdbNetworkManager: Turn to a singleton
    Change-Id: If0e40b5996137020ea9ac0100a769239e2fb59e3

--- sdk/src/java/org/lineageos/internal/tv/AdbNetworkManager.java
+++ sdk/src/java/org/lineageos/internal/tv/AdbNetworkManager.java
@@ -31,16 +31,23 @@ public class AdbNetworkManager {
     private static final String ADB_NOTIFICATION_CHANNEL_ID_TV = "usbdevicemanager.adb.tv";
 
     private Context mContext;
-    private boolean mRunning = false;
     private String mHostAddress = null;
     private NotificationManager mNotificationManager;
 
-    public AdbNetworkManager(Context context) {
+    private static AdbNetworkManager sInstance;
+
+    private AdbNetworkManager(Context context) {
         mContext = context;
         mNotificationManager = (NotificationManager)
                 mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         createNotificationChannel();
-        mRunning = SystemProperties.getInt(ADB_PORT_PROP, -1) > 0;
+    }
+
+    public static AdbNetworkManager getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new AdbNetworkManager(context);
+        }
+        return sInstance;
     }
 
     public void setEnabled(boolean enable) {
@@ -53,7 +60,7 @@ public class AdbNetworkManager {
     }
 
     public void start() {
-        if (mRunning) return;
+        if (isRunning()) return;
 
         SystemProperties.set(ADB_PORT_PROP, ADB_NETWORK_PORT);
 
@@ -68,7 +75,6 @@ public class AdbNetworkManager {
                     if (!addr.isLoopbackAddress() &&
                         addr.getHostAddress().indexOf(':') < 0) {
                         mHostAddress = addr.getHostAddress();
-                        break;
                     }
                 }
             }
@@ -78,26 +84,27 @@ public class AdbNetworkManager {
         };
 
         Log.d(TAG, "ADB over network enabled");
-        createNotification();
-        mRunning = true;
+        notifyAdbNetworkStarted();
     }
 
     public void stop() {
-        if (!mRunning) return;
+        if (!isRunning()) return;
 
         SystemProperties.set(ADB_PORT_PROP, "-1");
         cancelNotification();
         Log.d(TAG, "ADB over network disabled");
-        mHostAddress = null;
-        mRunning = false;
     }
 
     public boolean getEnabled() {
-        return mRunning;
+        return isRunning();
     }
 
     public String getHostAddress() {
         return mHostAddress;
+    }
+
+    private boolean isRunning() {
+        return SystemProperties.getInt(ADB_PORT_PROP, -1) > 0;
     }
 
     private void createNotification() {
