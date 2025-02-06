bc15481e20bdf1f2366579a9135290019ddbd99a
althafvly <althafvly@gmail.com>
2025-02-06 11:15:19 +0530

    sdk: Avoid TooManyRequestsException SystemUI crash
    - Fixes https://katb.in/vetiveyoqoh
    
    Change-Id: Ibdcad1ccdd681bec532e457e33db8e0618c6c39c

--- sdk/src/java/org/lineageos/internal/statusbar/NetworkTraffic.java
+++ sdk/src/java/org/lineageos/internal/statusbar/NetworkTraffic.java
@@ -365,7 +365,12 @@ public class NetworkTraffic extends TextView {
                 }
             };
 
-            mConnectivityManager.registerDefaultNetworkCallback(mDefaultNetworkCallback);
+            try {
+                mConnectivityManager.registerDefaultNetworkCallback(mDefaultNetworkCallback);
+            } catch (RuntimeException e) {
+                // ConnectivityManager.TooManyRequestsException
+                Log.e(TAG, "Too many requests made to the ConnectivityManager", e);
+            }
         }
     }
 
