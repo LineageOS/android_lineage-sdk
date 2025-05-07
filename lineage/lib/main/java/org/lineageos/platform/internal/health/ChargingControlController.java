05e3e33aa0a130392724c1014450a8a4b9cb7c3a
Luofan Chen <clfbbn@gmail.com>
2025-05-07 17:14:05 +0000

    health: Fix limit notification
    Change-Id: Iacea2006d35391438a28d9276b2be69c2d54c822

--- lineage/lib/main/java/org/lineageos/platform/internal/health/ChargingControlController.java
+++ lineage/lib/main/java/org/lineageos/platform/internal/health/ChargingControlController.java
@@ -424,7 +424,7 @@ public class ChargingControlController extends LineageHealthFeature {
 
         if (mode == MODE_LIMIT) {
             if (mCurrentProvider.update(mBatteryPct, limit)) {
-                mChargingNotification.post(limit, mBatteryPct == limit);
+                mChargingNotification.post(limit, mBatteryPct >= limit);
             }
         } else {
             ChargeTime chargeTime = getChargeTime();
