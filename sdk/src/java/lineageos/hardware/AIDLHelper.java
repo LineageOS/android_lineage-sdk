fc5dbbec0f6de70275df62543df89d4c13c2b334
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:44:11 +0200

    Wire up AIDL LiveDisplay HAL
    Change-Id: Ibfd8bf0ebba4171fd6ed619894ca6c0b75a0e50f

--- sdk/src/java/lineageos/hardware/AIDLHelper.java
+++ sdk/src/java/lineageos/hardware/AIDLHelper.java
@@ -5,9 +5,48 @@
 
 package lineageos.hardware;
 
+import android.util.Range;
+
 import java.util.ArrayList;
 
 class AIDLHelper {
+    static DisplayMode[] fromAIDLModes(vendor.lineage.livedisplay.DisplayMode[] modes) {
+        int size = modes.length;
+        DisplayMode[] r = new DisplayMode[size];
+        for (int i = 0; i < size; i++) {
+            vendor.lineage.livedisplay.DisplayMode m = modes[i];
+            r[i] = new DisplayMode(m.id, m.name);
+        }
+        return r;
+    }
+
+    static DisplayMode fromAIDLMode(vendor.lineage.livedisplay.DisplayMode mode) {
+        return new DisplayMode(mode.id, mode.name);
+    }
+
+    static HSIC fromAIDLHSIC(vendor.lineage.livedisplay.HSIC hsic) {
+        return new HSIC(hsic.hue, hsic.saturation, hsic.intensity,
+                hsic.contrast, hsic.saturationThreshold);
+    }
+
+    static vendor.lineage.livedisplay.HSIC toAIDLHSIC(HSIC hsic) {
+        vendor.lineage.livedisplay.HSIC h = new vendor.lineage.livedisplay.HSIC();
+        h.hue = hsic.getHue();
+        h.saturation = hsic.getSaturation();
+        h.intensity = hsic.getIntensity();
+        h.contrast = hsic.getContrast();
+        h.saturationThreshold = hsic.getSaturationThreshold();
+        return h;
+    }
+
+    static Range<Integer> fromAIDLRange(vendor.lineage.livedisplay.Range range) {
+        return new Range(range.min, range.max);
+    }
+
+    static Range<Float> fromAIDLRange(vendor.lineage.livedisplay.FloatRange range) {
+        return new Range(range.min, range.max);
+    }
+
     static TouchscreenGesture[] fromAIDLGestures(
             vendor.lineage.touch.Gesture[] gestures) {
         int size = gestures.length;
