6d5c8c24ead3d95ef17c9093c5d02ea841ae5f62
Michael Bestas <mkbestas@lineageos.org>
2026-01-18 02:17:30 +0200

    sdk: Make it REUSE compatible again
    Test: pipx run reuse lint
    Change-Id: I24916b0fe44a754b00d15cb3e1c4ec9af44d02e0

--- sdk/src/java/lineageos/app/NotificationGroup.java
+++ sdk/src/java/lineageos/app/NotificationGroup.java
@@ -1,6 +1,7 @@
-/**
+/*
  * SPDX-FileCopyrightText: 2012 The CyanogenMod Project
  * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package lineageos.app;
