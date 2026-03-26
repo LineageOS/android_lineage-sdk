c2324c7fe2e98b08d64d88bf1a1e5436e3c43cbf
grepfox <grepfox@tutamail.com>
2026-03-26 22:56:04 +0530

    sdk: Add an option to take screenshot using hw keys
    Change-Id: I4a979f38977023abf2380ef77cadfa463cc46630

--- sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
+++ sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
@@ -26,7 +26,8 @@ public class DeviceKeysConstants {
         LAST_APP,
         SPLIT_SCREEN,
         KILL_APP,
-        PLAY_PAUSE_MUSIC;
+        PLAY_PAUSE_MUSIC,
+        SCREENSHOT;
 
         public static Action fromIntSafe(int id) {
             if (id < NOTHING.ordinal() || id > Action.values().length) {
