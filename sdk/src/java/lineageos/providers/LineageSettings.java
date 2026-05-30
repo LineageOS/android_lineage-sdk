82216f7df95353e07ebb7e060a71d3884315ba93
LuK1337 <priv.luk@gmail.com>
2026-05-30 13:00:22 +0200

    sdk: Add partial screenshot action
    Change-Id: Ie364e7c9479577975e0c02c07cc3d50078cc0937

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -374,7 +374,7 @@ public final class LineageSettings {
             new InclusiveIntegerRangeValidator(0, 86400);
 
     private static final Validator sHardwareKeyActionvalidator =
-            new InclusiveIntegerRangeValidator(0, 12);
+            new InclusiveIntegerRangeValidator(0, 13);
 
     private static final Validator sAlwaysTrueValidator = new Validator() {
         @Override
@@ -1090,6 +1090,7 @@ public final class LineageSettings {
          * 10 - Kill foreground app
          * 11 - Play/pause music
          * 12 - Take screenshot
+         * 13 - Take partial screenshot
          */
         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
 
--- sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
+++ sdk/src/java/org/lineageos/internal/util/DeviceKeysConstants.java
@@ -27,7 +27,8 @@ public class DeviceKeysConstants {
         SPLIT_SCREEN,
         KILL_APP,
         PLAY_PAUSE_MUSIC,
-        SCREENSHOT;
+        SCREENSHOT,
+        PARTIAL_SCREENSHOT;
 
         public static Action fromIntSafe(int id) {
             if (id < NOTHING.ordinal() || id > Action.values().length) {
